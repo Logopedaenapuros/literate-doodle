7c98d5b43dca1f57f2759dd7ccb6b060cff3c08c
lologo <uo291560@uniovi.es>
2024-12-20 22:09:38 +0100

    Esto es mio y mejora

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express")
 
 let app =express ()
+
+app.get("/",(req,res) => {
+    res.send("hello word")
+})
 app.listen(8081,()=>{
    console.log("Tas en 8081 nun te pierdas") 
 })
+console.log("manuel")
+console.log("manuel")
