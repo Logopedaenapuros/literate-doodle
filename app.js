db2fbf38877c7f1875365059fa1ffc0348c701e1
lologo <uo291560@uniovi.es>
2024-12-22 16:46:06 +0100

    Descripción clara del cambio

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express")
 
 let app =express ()
 
+app.use(express.static("public"))
+
 app.get("/",(req,res) => {
     res.send("hello word")
+})
+app.get("/one",(req,res) => {
+   res.send("Me gustan las manzanas")
 })
 app.listen(8081,()=>{
    console.log("Tas en 8081 nun te pierdas") 
